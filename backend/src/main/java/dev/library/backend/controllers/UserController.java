c45683df2d84aa5bc309ca7c057ebe925d394a04
Reda Ganoutre <bganoutre52@agmail.com>
2025-02-05 17:33:05 +0100

    Added Services,RestControllers,WebConfig

--- backend/src/main/java/dev/library/backend/controllers/UserController.java
+++ backend/src/main/java/dev/library/backend/controllers/UserController.java
@@ -1,9 +1,15 @@
 package dev.library.backend.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,14 +20,39 @@ import dev.library.backend.services.UserService;
 @CrossOrigin
 @RequestMapping("/api/v1/users")
 public class UserController {
-    public final UserService userService;
     @Autowired
+    private UserService userService;
     public UserController(UserService userService) {
         this.userService = userService;
     }
-    @GetMapping("/{username}")
-    public User showUser(@PathVariable String username) {
-        return this.userService.getUser("sikrox.memer");
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
     }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User user) {
+        return userService.updateUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        ;
+    }
+    // @GetMapping("/{username}")
+    // public User showUser(@PathVariable String username) {
+    //     return this.userService.getUser("sikrox.memer");
+    // }
     
 }
