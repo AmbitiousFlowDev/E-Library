30a2f718cf5f6c3ed3d86aa7578b77620786df18
0xSikrox <simoarmymen9@gmail.com>
2025-02-07 01:28:07 +0100

    Removed Deperecated JWT dependency

--- backend/src/main/java/dev/library/backend/config/security/JwtService.java
+++ backend/src/main/java/dev/library/backend/config/security/JwtService.java
@@ -1,15 +1,23 @@
 package dev.library.backend.config.security;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+import dev.library.backend.constants.SecurityConstants;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
+
+// Note : PLEASE WORK ! I BEG OF YOU :(
 
 @Service
 public class JwtService {
     public String extractUsername(String token) {
         return null;
     }
-    private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey("secret").parseClaimsJws(token).getBody();
+    private Claim extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(this.getSignInKey()).parseClaimsJws(token).getBody();
+    }
+    private Key getSignInKey() {
+        byte[] keyByte = Base64.getDecoder().decode(SecurityConstants.JWT_SECRET);
+        return Keys
     }
 }
