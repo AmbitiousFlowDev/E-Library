8e4d85ad1a95182fc8bba2fd12771150a4f9ed1f
Reda Ganoutre <bganoutre52@agmail.com>
2025-02-15 17:06:11 +0100

    added file Images in resources

--- backend/target/generated-sources/annotations/dev/library/backend/dto/mappers/UserMapperImpl.java
+++ backend/target/generated-sources/annotations/dev/library/backend/dto/mappers/UserMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-02-15T14:37:28+0100",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
+    date = "2025-02-15T17:01:51+0100",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Amazon.com Inc.)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
