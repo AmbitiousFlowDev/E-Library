1e725d3ff002501cac4af364506fb5d2855fee2d
AmbitiousFlowDev <mohamed.lafrouh@gmail>
2025-05-09 19:39:08 +0100

    Added tests

---
+++ tests/src/test/java/BookSearchTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class BookSearchTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/chromedriver-win64/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testBookSearchAndClick() {
+        driver.get("http://localhost:5173/");
+        WebElement searchBar = driver.findElement(By.id("searchedBooks"));
+        searchBar.sendKeys("JavaScript For Dummies 3rd Edition");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("ul > li > a")));
+        List<WebElement> results = driver.findElements(By.cssSelector("ul > li > a"));
+        for (WebElement result : results) {
+            if (result.getText().contains("JavaScript For Dummies 3rd Edition")) {
+                result.click();
+                break;
+            }
+        }
+        String currentUrl = driver.getCurrentUrl();
+        Assertions.assertTrue(currentUrl.contains("/books/"), "The URL does not contain '/books/' as expected.");
+    }
+
+@AfterEach
+public void tearDown() {
+    if (driver != null) {
+        driver.quit();
+    }
+}
+}
+
